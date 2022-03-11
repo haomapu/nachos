7e22774260593c3c24dffce930d1356d736894fd
shinkyung <ngogiahai014@gmail.com>
2022-03-11 10:16:27 +0700

    final

--- nachos/nachos-3.4/code/test/sort.c
+++ nachos/nachos-3.4/code/test/sort.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
 
-void bubbleSort(int arr[], int n)
+void bubbleSortAsc(int arr[], int n)
 {
     int i, j, temp;
     for (i = 0; i < n-1; i++){
@@ -13,6 +13,21 @@ void bubbleSort(int arr[], int n)
         }
     }
 }
+
+void bubbleSortDes(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] < arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 void inputArray(int arr[], int n)
 {
     int i;
@@ -40,13 +55,25 @@ void printArray(int arr[], int n)
 int main()
 {
     int arr[10];
-    int n;
+    int n, mode;
     PrintString("Enter length of array: ");
     n = ReadNum();
     inputArray(arr, n);
     PrintString("Array: \n");
     printArray(arr, n);
-    bubbleSort(arr, n);
-    PrintString("Array is sorted: \n");
+    PrintString("1. Ascending\n");
+    PrintString("2. Descending\n");
+    PrintString("Enter mode: ");
+    mode = ReadNum();
+    if(mode == 1){
+        bubbleSortAsc(arr, n);
+    }
+    else if(mode == 2){
+        bubbleSortDes(arr, n);
+    }
+    else{
+        PrintString("Error!\n");
+    }
+    PrintString("\nArray is sorted: \n");
     printArray(arr, n);
 }
